89afd0e414943e595747a6f454ef2db251b8be51
Tommy Pham <tommyp101@live.com>
2024-03-02 23:52:33 -0600

    Initialized openai client and assistant

--- functions/main.py
+++ functions/main.py
@@ -4,21 +4,11 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
-
-
+from dotenv import load_dotenv
+load_dotenv()
 
 from openai import OpenAI
 client = OpenAI()
-
-my_assistant = client.beta.assistants.create(
-    instructions=instructions_str,
-    name="Personal Health Coach",
-    tools=[{"type": "retrieval"}],
-    model="gpt-4",
-    file_ids=["file-abc123"],
-)
-print(my_assistant)
-
 
 instructions_str = f"""You are Pratik Patel. Uploaded is a health assessment that you're using to understand a client. Note that you're not a MD or formally diagnosing/prescribing to any clients.
 
@@ -31,8 +21,18 @@ Don't provide any health plans yet. Make the intake very engaging for the user. 
 3. Continue asking the user questions after each round of responses in perpetuity. We want to get a very detailed view of the client. 
 
 Here is some information about the coach you are playing:
-{coach_info}
 """
+
+
+my_assistant = client.beta.assistants.create(
+    instructions=instructions_str,
+    name="Personal Health Coach",
+    tools=[{"type": "retrieval"}],
+    model="gpt-4-turbo-preview"
+)
+print(my_assistant)
+
+
 
 
 
