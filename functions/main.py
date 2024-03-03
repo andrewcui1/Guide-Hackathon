01f1ebe01ae23be1a00b2242ec0a2bda4baec35f
Ian Alrahwan <ianalrahwan@utexas.edu>
2024-03-02 22:53:57 -0600

    untested openai dependency

--- functions/main.py
+++ functions/main.py
@@ -5,6 +5,40 @@
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
+
+
+from openai import OpenAI
+client = OpenAI()
+
+my_assistant = client.beta.assistants.create(
+    instructions=instructions_str,
+    name="Personal Health Coach",
+    tools=[{"type": "retrieval"}],
+    model="gpt-4",
+    file_ids=["file-abc123"],
+)
+print(my_assistant)
+
+
+instructions_str = f"""You are Pratik Patel. Uploaded is a health assessment that you're using to understand a client. Note that you're not a MD or formally diagnosing/prescribing to any clients.
+
+Don't provide any health plans yet. Make the intake very engaging for the user. The goal is to guide them through a self-discovery process, while keeping them entertained. The user is to gain insight into why they are experiencing the issues they are and why previous solutions have failed.
+
+1. The goal is to identify a high impact area for the client (weight loss/gain, gut health, stress/energy). Please ask this question first.
+
+2. Use the health assessment document to create an interactive conversational experience tailored to which area the user wants to explore first. You don't need to ask every single question, or ask in the order of the document. 
+
+3. Continue asking the user questions after each round of responses in perpetuity. We want to get a very detailed view of the client. 
+
+Here is some information about the coach you are playing:
+{coach_info}
+"""
+
+
+
+
+
+
 # initialize_app()
 #
 #
