c1a27fd14914c1c685dad88e7c1bbae4bd99de55
willgbryan <will.bryan421@gmail.com>
2024-03-16 11:30:01 -0500

    moving things around // helper functions

--- AWShealthCoach/make_assistant.py
+++ AWShealthCoach/make_assistant.py
@@ -1,42 +1,53 @@
+from litellm import completion
+import os
+from typing import List, Dict
 
+# from dotenv import load_dotenv
+# load_dotenv()
 
-from dotenv import load_dotenv
-load_dotenv()
+# from openai import OpenAI
+# client = OpenAI()
 
-from openai import OpenAI
-client = OpenAI()
+"""
+Litellm: https://github.com/BerriAI/litellm allows us to retain the OpenAI API format but support different models.
 
-instructions_str = """Uploaded is a health assessment that you're using to understand a client. Note that you're not a MD or formally diagnosing/prescribing to any clients. 
+Example completion method: 
 
-You are pratik patel (a real former nutrition coach for the new york giants). Please keep your responses concise and enthusiastic. Each communication should be optimized to be sent over SMS with a max of 160 characters per output. Please bring up concrete examples of client success stories where appropriate. Keep everything to 8th grade level language complexity. Try to sound less like a GPT, more like a human. When asking questions, provide 2-4 choices, instead of leaving it open-ended.
+messages = [{ "content": "Hello, how are you?","role": "user"}]
 
-Begin your first response by explaining the premise behind the conversation and that insights uncovered during this process will aid Pratik in creating an action plan for the user. We are here to help the user fix their chronic, unresolved health issues. The goal is to identify major areas of improvement for the user (for example: weight loss/gain, gut health, stress/energy), sources of their successes/failures thus far, their environmental constraints, and information about their physiology.  Use the Pratik Health Assessment document as a basis for a conversational experience. Ask questions in the order of the document. Add original questions of your own as appropriate. 
-
-After every 5 responses the user sends, synthesize any insights for the user. Our goal is to aid them in self-discovery during this initial phase of their health journey. Remind that your effort here is informing Pratik's plan. Present in the 250 characters or less. Speak in an emotionally visceral, provocative way. The user should feel the pain of their struggles, yet empathize for themselves and the suffering and struggle they've endured so far.
-
-Do NOT provide recommended actions in your response. If the user asks for a suggestion or recommendation, explain that Pratik will help create your personalized plan.
+response = completion(model="gpt-3.5-turbo", messages=messages)
 """
 
-# Upload the document
-file = client.files.create(
-  file=open("Health-Goals-and-Recommendation-Template.docx", "rb"),
-  purpose='assistants'
-)
+# redundancy
+def check_env_key():
+  if "OPENAI_API_KEY" not in os.environ:
+    import yaml
+    with open("AWShealthCoach/template.yaml", "r") as file:
+      template_data = yaml.safe_load(file)
+      os.environ["OPENAI_API_KEY"] = template_data["Resources"]["TwilioWebhookFunction"]["Properties"]["Environment"]["Variables"]["OPENAI_API_KEY"]
+  if "OPENAI_API_KEY" not in os.environ:
+    raise EnvironmentError("OPENAI_API_KEY is not set in environment variables.")
 
-my_assistant = client.beta.assistants.create(
-    instructions=instructions_str,
-    name="Guide v3",
-    tools=[{"type": "retrieval"}],
-    model="gpt-4-turbo-preview",
-    file_ids=[file.id]
-)
-print(my_assistant)
+# putting messages first so we can support a default model
+def generate_response(messages: List[Dict[str, str]], model: str = "gpt-4-0125-preview"):
+  check_env_key()
+  response = completion(messages=messages, model=model)
+  return response
 
+# # Upload the document
+# file = client.files.create(
+#   file=open("Health-Goals-and-Recommendation-Template.docx", "rb"),
+#   purpose='assistants'
+# )
 
-
-
-
-
+# my_assistant = client.beta.assistants.create(
+#     instructions=instructions_str,
+#     name="Guide v3",
+#     tools=[{"type": "retrieval"}],
+#     model="gpt-4-turbo-preview",
+#     file_ids=[file.id]
+# )
+# print(my_assistant)
 
 
 # initialize_app()
