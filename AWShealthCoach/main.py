6cdd7264774e6940336ad5e8c00a0131b73859d9
willgbryan <will.bryan421@gmail.com>
2024-03-16 14:08:56 -0500

    add todo

--- AWShealthCoach/main.py
+++ AWShealthCoach/main.py
@@ -24,6 +24,8 @@ def ingest_documents(file_paths: List[str]) -> List[Document]:
 def pratik_patel_response(user_message: str, embedding_provider: str = "openai") -> str:
 
     instruct_prompt = pratik_patel_instruct()
+
+    #TODO probably need to parameterize file inputs
     documents = ingest_documents(['text_corpus/survey_questions_and_answers.pdf'])
     memory = Memory(embedding_provider)
     context = ContextCompressor(documents=documents, embeddings=memory.get_embeddings())
