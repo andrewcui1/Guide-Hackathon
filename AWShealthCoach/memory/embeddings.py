05fd11c4ced939fddfbebd7b96509edef722d19d
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-03-17 11:56:56 -0500

    Revert "Revert "Feature/support-dynamic-retrieval""

---
+++ AWShealthCoach/memory/embeddings.py
@@ -0,0 +1,25 @@
+from langchain.vectorstores import FAISS
+
+
+class Memory:
+    def __init__(self, embedding_provider, **kwargs):
+
+        _embeddings = None
+        match embedding_provider:
+            case "ollama":
+                from langchain.embeddings import OllamaEmbeddings
+                _embeddings = OllamaEmbeddings(model="llama2")
+            case "openai":
+                from langchain.embeddings import OpenAIEmbeddings
+                _embeddings = OpenAIEmbeddings()
+            case "huggingface":
+                from langchain.embeddings import HuggingFaceEmbeddings
+                _embeddings = HuggingFaceEmbeddings()
+
+            case _:
+                raise Exception("Embedding provider not found.")
+
+        self._embeddings = _embeddings
+
+    def get_embeddings(self):
+        return self._embeddings
